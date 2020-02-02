65195bc6eba573da0441338d4c2b6df58c1a2d77
Carlos Diaz <cdiaz86@gmail.com>
2020-02-01 22:58:17 -0500

    Adds labels to input fields.

--- src/components/EditCourseForm.js
+++ src/components/EditCourseForm.js
@@ -14,36 +14,46 @@ class EditCourseForm extends React.Component {
     return (
       <div className="course-edit">
         <p>Click anywhere outside of the form done editing.</p>
-      <input
-        type="text"
-        name="nameRef"
-        onChange={this.handleChange}
-        defaultValue={this.props.entry.nameRef} 
-      />
-      <input
-        type="text"
-        name="stuIdRef"
-        onChange={this.handleChange}
-        defaultValue={this.props.entry.stuIdRef} 
-      />
-      <input
-        type="text"
-        name="commentRef"
-        onChange={this.handleChange}
-        defaultValue={this.props.entry.commentRef} 
-      />
-      <input
-        type="text"
-        name="externalCourseRef"
-        onChange={this.handleChange}
-        defaultValue={this.props.entry.externalCourseRef} 
-      />
-      <input
-        type="text"
-        name="internalCourseRef"
-        onChange={this.handleChange}
-        defaultValue={this.props.entry.internalCourseRef} 
-      />
+      <p>Name:
+        <input
+          type="text"
+          name="nameRef"
+          onChange={this.handleChange}
+          defaultValue={this.props.entry.nameRef} 
+        />
+      </p>
+      <p> ID Number:
+        <input
+          type="text"
+          name="stuIdRef"
+          onChange={this.handleChange}
+          defaultValue={this.props.entry.stuIdRef} 
+        />
+      </p>
+      <p>External Course:
+        <input
+          type="text"
+          name="externalCourseRef"
+          onChange={this.handleChange}
+          defaultValue={this.props.entry.externalCourseRef} 
+        />
+      </p>
+      <p>Internal Course:
+        <input
+          type="text"
+          name="internalCourseRef"
+          onChange={this.handleChange}
+          defaultValue={this.props.entry.internalCourseRef} 
+        />
+      </p>
+      <p>Comment:
+        <input
+          type="text"
+          name="commentRef"
+          onChange={this.handleChange}
+          defaultValue={this.props.entry.commentRef} 
+        />
+      </p>
     </div>
     )
   }
