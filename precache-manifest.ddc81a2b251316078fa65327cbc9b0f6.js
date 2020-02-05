8d35a8de7073fae96bbd75adaad2cb41a7e8ceed
Carlos Diaz <cdiaz86@gmail.com>
2020-02-04 19:35:01 -0500

    Updates

---
+++ precache-manifest.ddc81a2b251316078fa65327cbc9b0f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68613f02fef19767e3483e81ce68919d",
+    "url": "/cat-tracker/index.html"
+  },
+  {
+    "revision": "7a8479498e0407875f99",
+    "url": "/cat-tracker/static/css/main.9945a755.chunk.css"
+  },
+  {
+    "revision": "24b95439a4ec253413dc",
+    "url": "/cat-tracker/static/js/2.027c66ec.chunk.js"
+  },
+  {
+    "revision": "ce300fdfce4eb542837865e4f605745f",
+    "url": "/cat-tracker/static/js/2.027c66ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a8479498e0407875f99",
+    "url": "/cat-tracker/static/js/main.443f4feb.chunk.js"
+  },
+  {
+    "revision": "0709f2f0b9ded7ed9489",
+    "url": "/cat-tracker/static/js/runtime-main.51623a81.js"
+  },
+  {
+    "revision": "7af9293dc3123ddfbcfe53def2591820",
+    "url": "/cat-tracker/static/media/catpaw.7af9293d.png"
+  }
+]);
